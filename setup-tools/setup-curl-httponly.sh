8df6b5e9f7084952a08de76ba8236b36fc590261
Gianluca Nitti <gianluca.nitti96@gmail.com>
2016-02-08 15:59:04 +0100

    Changed curl setup script to build libcurl only.

--- setup-tools/setup-curl-httponly.sh
+++ setup-tools/setup-curl-httponly.sh
@@ -8,8 +8,9 @@ DEST="$DIR/../nwjsmanager/lib"
 
 FLAGS="--disable-debug --disable-rtsp --disable-smtp --disable-gopher --disable-pop3 --disable-imap --disable-ftp --disable-file --disable-ldsp --disable-telnet --disable-dict --disable-tftp --disable-ares --disable-cookies --disable-crypto-auth --disable-ipv6 --disable-manual --disable-proxy --disable-unix-sokets --disable-ldap --disable-ldaps --disable-verbose --disable-versioned-symbols --enable-hidden-symbols --without-libidn --without-librtmp --without-ssl --without-zlib"
 
-mkdir -p $DIR/../nwjsmanager/lib/src
-cd $DIR/../nwjsmanager/lib/src
+SRCDIR=$DIR/../nwjsmanager/lib/src
+mkdir -p $SRCDIR
+cd $SRCDIR
 if [ ! -f ./curl-src/buildconf ]; then
 	echo "Downloading curl source code..."
 	git clone https://github.com/curl/curl.git curl-src
@@ -22,12 +23,15 @@ fi
 
 echo "Building for linux64..."
 ./configure --prefix=$DEST/linux64/curl $TYPE $FLAGS
-make install
+cd $SRCDIR/curl-src/lib && make install
+cd ..
 
 echo "Building for linux32..."
 ./configure --prefix=$DEST/linux32/curl $TYPE $FLAGS CFLAGS=-m32
-make install
+cd $SRCDIR/curl-src/lib && make install
+cd ..
 
 echo "Building for win32..."
 ./configure --prefix=$DEST/win32/curl --host=i686-w64-mingw32 $TYPE $FLAGS
-make install
+cd $SRCDIR/curl-src/lib && make install
+cd ..
