69bf788e55fc2d1fd13428f636bbd56d381faeb2
Gianluca Nitti <gianluca.nitti96@gmail.com>
2016-03-11 23:06:54 +0100

    Fixed the memory issue that was breaking the build.
    Also cleaned up nwjsBinaryCache.c, using the string_concat function from strUtil.c.

--- nwjsmanager/nwjsBinaryCache.c
+++ nwjsmanager/nwjsBinaryCache.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include "paths.h"
 #include "nwjsBinaryCache.h"
+#include "strUtil.h"
 
 semverList_t nwjs_binary_cache_get_versions(){
 	semverList_t result = {NULL, 0};
@@ -16,15 +17,12 @@ semverList_t nwjs_binary_cache_get_versions(){
 	}
 	struct dirent *ent;
 	int allocatedSize = 10;
-	result.items = malloc(allocatedSize * sizeof(semver_t));
+	result.items = calloc(allocatedSize, sizeof(semver_t));
 	while((ent = readdir(d)) != NULL){
 		if(strcmp(ent->d_name, "..") == 0 || strcmp(ent->d_name, ".") == 0)
 			continue;
 		struct stat st;
-		char *filename = malloc((strlen(binDir) + strlen(ent->d_name) + 2) * sizeof(char)); //TODO: could be replace with an ad-hoc function, like the concat() used in applauncher
-		strcpy(filename, binDir);
-		//strcat(filename, "/");
-		strcat(filename, ent->d_name);
+		char *filename = string_concat(2, binDir, ent->d_name);
 		if(stat(filename, &st) != 0){
 			free(filename);
 			continue;
@@ -34,7 +32,7 @@ semverList_t nwjs_binary_cache_get_versions(){
 			result.count++;
 			if(result.count == allocatedSize)
 				result.items = realloc(result.items, (allocatedSize = result.count + 1) * sizeof(semver_t));
-		}else if(&result.items[result.count])
+		}else
 			semver_free(&result.items[result.count]);
 	}
 	closedir(d);
