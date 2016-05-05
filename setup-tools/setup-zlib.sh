1b98bea38c44deaa6bf4732baa2a2ba9210b891d
Gianluca Nitti <gianluca.nitti96@gmail.com>
2016-05-05 22:08:07 +0200

    Linking zlib statically on Linux too.

--- setup-tools/setup-zlib.sh
+++ setup-tools/setup-zlib.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -e
-#This script will download the zlib source from the official repository and compile it for the win32 target (Linux binaries are already installed).
+#This script will download the zlib source from the official repository and compile it for the target platforms.
 
+VERSION=v1.2.8
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEST="$DIR/../nwjsmanager/lib"
 
@@ -11,10 +12,22 @@ if [ ! -f ./zlib-src/Makefile ]; then
 	echo "Downloading zlib source code..."
 	git clone https://github.com/madler/zlib.git zlib-src
 fi
-
 cd zlib-src
-#TODO: checkout version tag
+git checkout tags/$VERSION
+
+echo "Building for linux64..."
+./configure --static --prefix=$DEST/linux64/zlib
+make install
+make clean
+
+echo "Building for linux32..."
+export CC="gcc -m32"
+./configure --static --prefix=$DEST/linux32/zlib
+make install
+make clean
+
+echo "Building for win32..."
 patch win32/Makefile.gcc $DIR/zlibMakefile.patch
-INSTALLDIR=$DIR/../nwjsmanager/lib/win32/zlib
+INSTALLDIR=$DEST/win32/zlib
 mkdir -p $INSTALLDIR
 BINARY_PATH=$INSTALLDIR/bin INCLUDE_PATH=$INSTALLDIR/include LIBRARY_PATH=$INSTALLDIR/lib make -f win32/Makefile.gcc install
